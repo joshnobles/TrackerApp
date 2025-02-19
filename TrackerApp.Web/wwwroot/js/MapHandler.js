988f45dfb963bc4b34749f9a3b8eddcfb0706be9
Josh Nobles <noblesj002@gmail.com>
2025-02-18 23:55:26 -0500

    Added sample data as proof of concept

--- TrackerApp.Web/wwwroot/js/MapHandler.js
+++ TrackerApp.Web/wwwroot/js/MapHandler.js
@@ -16,3 +16,40 @@ const showPopup = event => {
 }
 
 map.on('click', showPopup);
+
+const sampleLocations = [
+    [41.2583, -77.0463],
+    [41.2518, -77.0458],
+    [41.2448, -77.0488],
+    [41.2429, -77.0446],
+    [41.2419, -77.0378],
+    [41.2411, -77.0342],
+    [41.2376, -77.0323],
+    [41.2374, -77.0277],
+    [41.2376, -77.0230],
+    [41.2336, -77.0226],
+    [41.2335, -77.0258]
+];
+
+let polyLine = L.polyline([], { color: 'red' });
+polyLine.addTo(map);
+
+let icon = L.divIcon({ className: 'bi bi-person-standing' });
+
+let marker = L.marker(sampleLocations[0], { icon: icon });
+marker.addTo(map);
+
+let i = 0;
+
+const displaySampleData = () => {
+    if (!sampleLocations[i]) {
+        polyLine.setLatLngs([]);
+        i = 0;
+    }
+
+    polyLine.addLatLng(sampleLocations[i]);
+
+    marker.setLatLng(sampleLocations[i++]);
+}
+
+const interval = setInterval(displaySampleData, 1000);
