bfe108b9297333854c91e35402cf3c9114a97a0f
Josh Nobles <noblesj002@gmail.com>
2025-03-19 21:27:07 -0400

    Added change to redirect authenticated users to map page.

--- TrackerApp.Web/Pages/Index.cshtml.cs
+++ TrackerApp.Web/Pages/Index.cshtml.cs
@@ -1,9 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace TrackerApp.Web.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet() { }
+        public IActionResult OnGet() 
+        {
+            if (User.Identity is not null && User.Identity.IsAuthenticated)
+                return Redirect("/Private/Map");
+
+            return Page();
+        }
     }
 }
