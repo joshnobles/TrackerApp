dad38804f9414686fc4f9557e235a590138cb969
Josh Nobles <noblesj002@gmail.com>
2025-04-10 23:50:17 -0400

    Edited max zoom for water color tile layer.

--- TrackerApp.Web/wwwroot/js/MapHandler.js
+++ TrackerApp.Web/wwwroot/js/MapHandler.js
@@ -139,7 +139,7 @@
             }),
             'Water Color': L.tileLayer('https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.{ext}', {
                 minZoom: 1,
-                maxZoom: 16,
+                maxZoom: 13,
                 attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
                 ext: 'jpg'
             }),
